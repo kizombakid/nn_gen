e600f45be3963b39a3a7a2146756defafd60a90e
Oscar Alves <oscar@pika.tpgi.com.au>
2020-02-08 13:58:52 +1100

    new version of tools

--- nn_tools.py
+++ nn_tools.py
@@ -1,33 +1,79 @@
 
 from fts_tools import *
+from sql_tools import *
 import sqlite3
-import numpy as np
-import datetime
 import os
+import pyqtgraph as pg
+from pyqtgraph import QtCore, QtGui
+from pathlib import Path
+
+
+class NN_Def():
+
+    def __init__(self, name, dir=""):
+
+        self.exp = name
+
+        home = str(Path.home())
+        if dir == "": dir = home + '/analyse/nn_exps/' + name
+
+        file = open(dir + "/def.txt", "r")
+
+        self._defaults()
+        self.basedir = dir
+
+        for line in file:
+
+            if '=' in line:
+
+                line = line.replace(" ", "").split("=")
+                if 'network' in line[0]:
+                    exec('self.' + line[0] + '.append(' + str(line[1]) + ')')
+                else:
+                    exec('self.' + line[0] + '=' + str(line[1]))
+
+    def _defaults(self):
+
+        self.network1A = []
+        self.network1B = []
+        self.network2 = []
+        self.epochs = 1
+        self.lookback = 1
+        self.batchsize = 1
+        self.rmsprop = 0
+        self.patience = 1
+        self.optimizer = 'rmsprop'
+
 
 
 class SPriceEod():
 
-    def __init__(self,code,date,daysback=200):
+    def __init__(self,code,date,daysback=200, date2=None, daysforward=0):
         if code == 'dj' or code == 'asx200':
             self.read_index(code, date, daysback=daysback)
         else:
-            self.read_eod(code, date, daysback=daysback)
+            self.read_eod(code, date, daysback=daysback,daysforward=daysforward, date2=date2)
 
         self.get_indicators()
 
         return
 
-    def read_eod(self,code,date,daysback=200):
+    def read_eod(self,code,date,daysback=200,daysforward=0, date2=None):
         basedir = get_basedir()
         sqlfile = "eod_main_code.db"
+        print (sqlfile)
         sqlfilei = os.path.join(basedir, 'data', sqlfile)
         conni = sqlite3.connect(sqlfilei)
         curi = conni.cursor()
-        date0 = date_add(date, -daysback)
-        date2 = date
 
-        prices = sql_get_codedates(curi, code, date0, date2, datetype='source')
+        if date2 != None:
+            date0 = min(date,date2)
+            date1 = max(date,date2)
+        else:
+            date0 = date_add(date, -daysback)
+            date1 = date_add(date, daysforward)
+
+        prices = sql_get_codedates(curi, code, date0, date1, datetype='source')
         self.date = prices['date']
         self.open = prices['open']
         self.high = prices['high']
@@ -62,7 +108,8 @@ class SPriceEod():
 
 
 def sql_rd_eod_index(date, code,daysback=200):
-    sqldir = '/Users/oalves/python/nn/nn_gen'
+    home = str(Path.home())
+    sqldir = home+'/python/nn/nn_gen'
     sqlfile = os.path.join(sqldir, code + '.db')
     print (sqlfile)
 
@@ -100,4 +147,57 @@ def sql_rd_eod_index(date, code,daysback=200):
 
     return rdate[-nd:], ropen[-nd:], rhigh[-nd:], rlow[-nd:], rclose[-nd:], rvolume[-nd:]
 
+
+class CandlestickItem(pg.GraphicsObject):
+    ## Create a subclass of GraphicsObject.
+    ## The only required methods are paint() and boundingRect()
+    ## (see QGraphicsItem documentation)
+    def __init__(self, data,nvals=''):
+        pg.GraphicsObject.__init__(self)
+     #   self.data = data  ## data must have fields: time, open, close, min, max
+        if nvals == '':
+            nv=len(data.index)
+        else:
+            nv=nvals
+        self.generatePicture(data,nvals=nv)
+
+    def generatePicture(self,data,nvals=''):
+
+        ## pre-computing a QPicture object allows paint() to run much more quickly,
+        ## rather than re-drawing the shapes every time.
+        self.picture = QtGui.QPicture()
+        p = QtGui.QPainter(self.picture)
+        p.setPen(pg.mkPen('k', width=2))
+        w = (data.index[1] - data.index[0]) / 3.
+        if nvals == '':
+            nv=len(data.index)
+        else:
+            nv=nvals
+        for i in range(0,nv):
+            t = data.index[i]
+            open = data.open[i]
+            vmax = data.high[i]
+            vmin = data.low[i]
+            close = data.close[i]
+
+
+            if vmin != vmax : p.drawLine(QtCore.QPointF(t, vmin), QtCore.QPointF(t, vmax))
+            if open > close:
+                p.setBrush(pg.mkBrush('r'))
+            else:
+                p.setBrush(pg.mkBrush('g'))
+            p.drawRect(QtCore.QRectF(t - w, open, w * 2, close - open))
+
+         #p.end()
+
+        return
+
+    def paint(self, p, *args):
+        p.drawPicture(0, 0, self.picture)
+
+    def boundingRect(self):
+        ## boundingRect _must_ indicate the entire area that will be drawn on
+        ## or else we will get artifacts and possibly crashing.
+        ## (in this case, QPicture does all the work of computing the bouning rect for us)
+        return QtCore.QRectF(self.picture.boundingRect())
 
